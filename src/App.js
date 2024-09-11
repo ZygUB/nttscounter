965ef57fb9f2a99ad22bdc9dc9fe78957e08e53b
ZygUB <zygvlogs1@gmail.com>
2024-09-11 23:26:40 +0300

    First  Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [totalViews, setTotalViews] = useState(() => {
+    const storedViews = localStorage.getItem('totalViews');
+    return storedViews ? parseInt(storedViews) + 1 : 1;
+  })
+
+  useEffect(() => {
+    localStorage.setItem('totalViews', totalViews);
+  }, [totalViews]);
+  
+  return (
+    <div className="App">
+      <h1>Welcome to NTTS Fans Visitor Website</h1>
+      <h2> You are {totalViews} viewer!</h2>
+    </div>
+  );
+}
+export default App
