c5be44bda65280ad93444143c0a02e1c548da7c4
ZygUB <166707799+ZygUB@users.noreply.github.com>
2024-09-12 01:18:17 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 
 function App() {
-  const [totalViews, setTotalViews] = useState(0);
+  const [totalViews] = useState(() => {
+    const storedViews = localStorage.getItem('totalViews');
+    return storedViews ? parseInt(storedViews) + 1 : 1;
+  })
 
-  useEffect(() => {
-    axios.get('/increment-view-count')
-      .then((res) => res.data)
-      .then((data) => setTotalViews(data.totalViews));
-  }, []);
-
-  return (
-    <div className="App">
-      <h1>Welcome to NTTS Fans Visitor Website</h1>
-      <h2> You are {totalViews} viewer!</h2>
-    </div>
-  );
+  useEffect(() => {
+    localStorage.setItem('totalViews', totalViews);
+  }, [totalViews]);
+  
+  return (
+    <div className="App">
+      <h1>Welcome to NTTS Fans Visitor Website</h1>
+      <h2> You are {totalViews} viewer!</h2>
+    </div>
+  );
 }
-export default App;
+export default App
